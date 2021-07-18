64d24eb98c4d69288fa5ab7ad1061b8debdf4f7c
Ketan Vishwakarma <ketan.vishwakarma@inventum.net>
2021-07-18 23:42:49 +0530

    Added Question statement to Match Array Program and refactoring

--- match_array.py
+++ match_array.py
@@ -1,3 +1,13 @@
+"""
+Question: Given two arrays determine wheather 2nd array is squared of first array. i.e.
+
+arr_1 = [1,2,3,4] 
+arr_2 = [1,4,9,16]
+
+arr_2 has all the elements of arr_1 and they are also squared in value. Also note order does not matter.
+"""
+
+
 class MatchArray:
     def __init__(self,array1,array2):
         self.array1=array1
@@ -7,29 +17,26 @@ class MatchArray:
         if len(self.array1) !=len(self.array2):
             return False
         array1_dict ={}
-        array2_dict = {}
         for item in self.array1:
             squared_item = item**2
             if squared_item in array1_dict.keys():
-                item_val = array1_dict.get(squared_item)
-                array1_dict.update({squared_item:item_val+1})
+                array1_dict.update({squared_item:(array1_dict.get(squared_item)+1)})
             else:
                 array1_dict.update({squared_item:1})
 
+        array2_dict = {}
         for item in self.array2:
             if item in array2_dict.keys():
-                item_val = array2_dict.get(item)
-                array2_dict.update({item:item_val+1})
+                array2_dict.update({item:(array2_dict.get(item)+1)})
             else:
                 array2_dict.update({item:1})
         
         for item in array1_dict.keys():
             if item not in array2_dict.keys():
                 return False
-            array_2_value = array2_dict.get(item)
-            if array_2_value != array1_dict.get(item):
+            if array2_dict.get(item) != array1_dict.get(item):
                 return False
         return True
 
-x = MatchArray([1,2,3,2,9], [1,4,9,4,25])
+x = MatchArray([1,2,3,2,9], [1,4,9,4,81])
 print(x.does_array_match())
