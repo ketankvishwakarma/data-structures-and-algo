1ce7399bb467952f988d36b9d6a21ee2686ccefd
Ketan Vishwakarma <ketan.vishwakarma@inventum.net>
2021-11-09 00:09:19 +0530

    refactored misc programs

--- misc/almost_increasing_sequence.py
+++ misc/almost_increasing_sequence.py
@@ -16,7 +16,7 @@ almostIncreasingSequence(sequence) = true.
 You can remove 3 from the array to get the strictly increasing sequence [1, 2]. Alternately, you can remove 2 to get the strictly increasing sequence [1, 3].
 """
 
-def almostIncreasingSequence(sequence):
+def almost_increasing_sequence(sequence):
     sequence_disturbed_count = 0
     current_max = sequence[0]
     for idx in range(len(sequence)-1):   
@@ -28,11 +28,11 @@ def almostIncreasingSequence(sequence):
             return False
     return True
 
-print(almostIncreasingSequence([1,3,2]))
+print(almost_increasing_sequence([10, 1, 2, 3, 4, 5]))
 
-print(almostIncreasingSequence([1,2,1,2]))
+print(almost_increasing_sequence([1,2,1,2]))
 
-print(almostIncreasingSequence([3, 6, 5, 8, 10, 20, 15]))
+print(almost_increasing_sequence([3, 6, 5, 8, 10, 20, 15]))
 
-print(almostIncreasingSequence([1, 1, 2, 3, 4, 4]))
+print(almost_increasing_sequence([1, 1, 2, 3, 4, 4]))
 
